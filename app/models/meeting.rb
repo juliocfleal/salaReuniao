d3dcee2f044c0b69d50401cba62fcbd624350ace
Julio Cesar Freitas Leal <julio.leal@alunos.ifsuldeminas.edu.br>
2022-03-16 07:30:11 -0300

    Make some validations on back-end(no two time same)

--- app/models/meeting.rb
+++ app/models/meeting.rb
@@ -1,3 +1,9 @@
 class Meeting < ApplicationRecord
   belongs_to :user
+  validates :name, presence: true
+  validates :start_time,
+            uniqueness: {
+              message: 'Este horario nao esta disponivel',
+            }
+            
 end
