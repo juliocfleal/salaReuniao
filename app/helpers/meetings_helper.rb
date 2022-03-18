2cc376a54ab4a4a5a0a1dc0973b66348d6c39805
Julio Cesar Freitas Leal <julio.leal@alunos.ifsuldeminas.edu.br>
2022-03-18 01:02:37 -0300

    Add some validations, prepare helper function

--- app/helpers/meetings_helper.rb
+++ app/helpers/meetings_helper.rb
@@ -1,2 +1,9 @@
 module MeetingsHelper
+    def listhours
+        i = 6
+        while i < 23 do
+            opa
+            i = i + 1 
+        end
+    end
 end
--- app/models/meeting.rb
+++ app/models/meeting.rb
@@ -5,5 +5,28 @@ class Meeting < ApplicationRecord
             uniqueness: {
               message: 'Este horario nao esta disponivel',
             }
-            
+validate :is_end_before
+validate :is_past
+validate :hour_range
+
+def is_end_before
+  if(start_time > end_time)
+    self.errors.add(:start_time, "O data do fim da reunião deve ser superior ao inicio.")
+  end 
+end        
+
+def is_past
+  if (start_time < Time.now)
+    self.errors.add(:start_time, "O agendamento só é possivel para datas futuras, com uma antecedencia minima de 3 horas.")
+  end
+end
+
+def hour_range
+  if(start_time.hour < 6)
+    self.errors.add(:start_time, "As salas de reunião estao disponiveis entre as 6 as 23 horas.")
+  end
+end
+
+
+
 end
